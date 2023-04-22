05f199d6580913ed254637b07a427fcca34b8e9c
Eva Camino <caminoeva1@gmail.com>
2023-04-22 10:31:34 -0600

    finishing seeds

---
+++ index.js
@@ -0,0 +1,141 @@
+require("console.table")
+const inquirer = require("inquirer");
+const mysql = require('mysql2');
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username,
+      port: 3306,
+      user: 'root',
+      // TODO: Add MySQL Password
+      password: 'password',
+      database: 'employee_db'
+    },
+  );
+
+db.connect((err) => {
+    if (err) throw err;
+    init();
+})
+
+const initialQuestion = [
+{
+    type: "list",
+    name: "choice",
+    message: "What do you want to do?",
+    choices: [
+    "View All Departments",
+    "View All Roles",
+    "View All Employees",
+    "Add a Department",
+    "Add a Role",
+    "Add an Employee",
+    "Update Employee Role",
+    ],
+},
+]
+
+async function init() {
+    try {
+        const answer = await inquirer.prompt(initialQuestion);
+        console.log(answer.choice);
+
+        // if answer is "View all Departments", then call viewAllDeplartments() function
+        // or, if answer is "View all Roles," then call viewAllRoles() function
+        if (answer.choice === "View All Departments"){
+            viewAllDepartments();
+        } 
+        else if (answer.choice === "View All Roles"){
+            viewAllRoles();
+        }
+        else if (answer.choice === "View All Employees"){
+            viewAllEmployees();
+        }
+        else if (answer.choice === "Add a Department"){
+            addDeparment();
+        }
+        else if (answer.choice === "Add a Role"){
+            addRole();
+        }
+        else if (answer.choice === "Add an Employee"){
+            addEmployee();
+        }
+        else {
+            updateEmployeeRole();
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+function viewAllDepartments() {
+
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) {
+            return reject(err);
+        }
+
+        console.table(res);
+        init();
+    })
+
+    // db.promise().query('SELECT * FROM department', function (err, [results]) {
+    //     console.table(results);
+    //   });
+    //console.log("function for view all departments")
+
+    // db.promise().query('SELECT * FROM department');
+
+}
+
+function viewAllRoles() {
+   
+    db.query('SELECT * FROM role', (err, res) => {
+        if (err) {
+            return reject(err);
+        }
+
+        console.table(res);
+        init();
+    })
+}
+function viewAllEmployees() {
+   
+    db.query('SELECT * FROM employee', (err, res) => {
+        if (err) {
+            return reject(err);
+        }
+
+        console.table(res);
+        init();
+    })
+}
+function addDeparment(){
+
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) {
+            return reject(err);
+        }
+
+        console.table(res);
+        init();
+    })
+}
+function addRole(){
+    console.log("function for add role")
+}
+
+function addEmployee(){
+    console.log("function for add employee")
+}
+
+function updateEmployeeRole(){
+    console.log("function for update employee role")
+}
+
+// init();
+// study react as a full stack developer
+// basic of mysql mostly
+// ask questions thru inquirer)
--- seed.sql
+++ seed.sql
@@ -1,11 +1,30 @@
 USE employee_db;
 
 INSERT INTO department (name) VALUES
+    ("Engineering"),
     ("Sales"),
-    ("Marketing"),
     ("Finance"),
     ("Legal")
-INsERT INTO role(title,salary,deparment_id)
+INSERT INTO role
+    (title, salary, department_id)
 VALUES
-()
+    ('Sales Lead', 100000, 1),
+    ('Accountant', 125000, 3),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Salesperson', 80000, 1),
+    ('Lawyer', 190000, 4);
+    ('Account Manager', 160000, 3),
+    ('Legal Team Lead', 250000, 4),
 
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Mike', 'Roberts', 2, 1),
+    ('Jared', 'Lee', 1, NULL),
+    ('Kerry', 'Dawn', 4, 3),
+    ('Max', 'Shaun', 5, NULL),
+    ('Derrick', 'Fisher', 7, NULL),
+    ('Tim', 'Arnold', 8, 7);
+    ('Mariana', 'Brown', 6, 5),
+    ('Selma', 'Taryn', 3, NULL),
